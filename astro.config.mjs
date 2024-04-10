4127653f01cca41edda47fb8c2140e5437e732ee
Isaac No <isaacyno@gmail.com>
2024-04-10 04:43:57 -0700

    Fixed broken links?

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://isaacyno.github.io/',
+  site: 'https://isaacyno.github.io',
   integrations: [mdx(), sitemap()]
 });
