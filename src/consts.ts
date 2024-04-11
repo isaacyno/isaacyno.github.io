d470dbab66942dc09143ac07a4076ad03126b5c0
Isaac No <isaacyno@gmail.com>
2024-04-10 22:35:13 -0700

    Changed global constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Isaac No | Home';
-export const SITE_DESCRIPTION = 'Personal Website, Blog';
+export const HOME_TITLE = 'Isaac No | Home';
+export const HOME_DESCRIPTION = "Hi, I'm Isaac! Welcome to my simple, personal website.";
+export const BLOG_TITLE = 'Isaac No | Blog';
+export const BLOG_DESCRIPTION = "Welcome to my personal blog! Here you'll find posts about all things math.";
